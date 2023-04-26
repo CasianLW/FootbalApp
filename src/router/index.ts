95f3e9382179690744c88c06267e7eb06856c431
CasianLW <ciorbacasian@gmail.com>
2023-04-26 16:42:29 +0200

    router selon la consigne

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,39 +1,33 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+// import TabsPage from "../views/TabsPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/tabs/tab1'
+    path: "/",
+    redirect: "/ligues",
   },
   {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
-  }
-]
+    path: "/ligues",
+    component: () => import("@/views/Ligues.vue"),
+  },
+  {
+    path: "/ligues/:id/classement",
+    component: () => import("@/views/Classement.vue"),
+  },
+  {
+    path: "/ligues/:id/equipes",
+    component: () => import("@/views/Equipes.vue"),
+  },
+  {
+    path: "/equipes/:id",
+    component: () => import("@/views/EquipeDetail.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
